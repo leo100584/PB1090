30004619146e793cd33b609425c325796be079a4
leo100584 <54706498+leo100584@users.noreply.github.com>
2022-05-03 11:46:58 +0200

    Refactoring Oppgave3.java
    Update in hentTestData()

--- eksamen/v2021/Oppgave3/Oppgave3.java
+++ eksamen/v2021/Oppgave3/Oppgave3.java
@@ -1,33 +1,52 @@
-import java.util.Scanner;
 class Oppgave3 {
-    //Main-metoden er ikke den del av løsningsforslaget:
+    // Main-metoden er ikke den del av løsningsforslaget:
     public static void main(String[] args) {
         System.out.println("Oppgave3 start");
 
-        Idrettslag medlemArray = hentTestData();
-        Medlem[] medlemsNummer = sortereMedlemer(medlemArray.medlemer);
+        Idrettslag medlemer = hentTestData();
+        Medlem[] medlemerMedNr = sortereMedlemer(medlemer.medlemer);
 
-        for (Medlem medlem : medlemsNummer) {
-            System.out.println(medlem.hentMedlesnummer());
+        System.out.println("\nAlle medlemsnummmer:");
+
+        for (Medlem medlem : medlemerMedNr) {
+            System.out.print(medlem.hentMedlesnummer()+" ");
         }
 
-        //Tester mapping av medlemsNr i array; indeks0 = medlemsNr1600:
-        System.out.println("medlem på indeks 0 er nr "+medlemsNummer[0].hentMedlesnummer());
+        // Tester mapping av medlemsNr i array; indeks 0 = medlemsNr 1000:
+        int indeksTest = 0;
+        System.out.println("\nMedlem nummer:" + medlemerMedNr[indeksTest].hentMedlesnummer() + " ligger på indeks:" + indeksTest);
+        indeksTest = 385;
+        // Tester mapping av medlemsNr i array; indeks 385 har medlesmnummer 1385
+        System.out.println("Medlem nummer:" +medlemerMedNr[indeksTest].hentMedlesnummer() + " ligger på indeks:" + indeksTest);
+
+        // Sjekker evt. hva som kom på indeks 1500, som var null i opprinelig medlem array
+        //indeksTest = 1500;
+        //System.out.println("Medlemnummer: " +medlemsNummer[indeksTest].hentMedlesnummer() + " ligger på indeks:" + indeksTest);
 
         System.out.println("Oppgave3 ferdig");
     }
 
-    //Denne metoden er ikke den del av løsningsforslaget:
+    // Denne metoden er ikke den del av løsningsforslaget:
     public static Idrettslag hentTestData() {
-        int startNummer = 1000;
-        Idrettslag temp = new Idrettslag(2000);
+        int antallMedlemer = 2000;
+        Idrettslag temp = new Idrettslag(antallMedlemer);
 
-        for (int index = 0; index < temp.medlemer.length; index++) {
-            //Legger til medlems nr. på hver 7.person:
-            if (index % 7 == 0) {
-                temp.medlemer[index] = new Medlem("Person" + index, startNummer + index);
-            }
+        // Skal ha medlemsnummer fra 1000-2999
+        // Starter på 1000:
+        int medlemsNummer = 1000;
+
+        for (int index = 0; index < temp.medlemer.length; index++) {    
+            temp.medlemer[index] = new Medlem("Person" + index, + medlemsNummer);
+
+            medlemsNummer += 1;
+
         }
+
+        // Legger også inn noen null-refernaser for å teste sortereMedlemer:
+        temp.medlemer[1500] = null;
+        temp.medlemer[1600] = null;
+        temp.medlemer[1700] = null;
+
         return temp;
     }
 
@@ -36,19 +55,27 @@ class Oppgave3 {
         int antall = 0;
 
         for (Medlem medlem : medlemArray) {
+            
+            if(medlem == null){
+                    System.out.println("Hopper over medlem...");
+            }
+
             if (medlem != null) {
                 if (medlem.harMedlemsNr()) {
                     temp[antall] = medlem;
                     antall += 1;
+                    
                 }
             }
         }
 
-        Medlem[] medlemerMednr = new Medlem[antall];
-        medlemerMednr = kopierTilNyttArray(temp, antall);
-        //Alternativ losing kommentert ut under
-        //System.arraycopy(temp,0,medlemerMednr,0,antall);
-        return medlemerMednr;
+        Medlem[] medlemerMedNr = new Medlem[antall];
+        medlemerMedNr = kopierTilNyttArray(temp, antall);
+
+        //Alternativ løsing med java sin System.arraycopy istendenfor for å bruke hjelpe-metoden; kopierTilNyttArray
+        //System.arraycopy(temp,0,medlemerMedNr,0,antall);
+
+        return medlemerMedNr;
     }
 
     public static Medlem[] kopierTilNyttArray(Medlem[] temp, int antall){
@@ -80,7 +107,7 @@ class Medlem {
     }
 }
 
-//Denne klassen trenger ikke være med i løsningen, men den blir bruk i dette løsningsforslaget for å teste Medlems-metodene.
+// Denne klassen trenger ikke være med i løsningen, men den blir bruk i dette løsningsforslaget for å teste sortereMedlemer.
 class Idrettslag {
     private int lengde;
     private int antall;
